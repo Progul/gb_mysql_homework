389cd184b3ed906ecbb24abd756140cf7ec1a2c6
Progul <desnenko@ya.ru>
2022-03-20 21:45:53 +0300

    added homework

--- homework_6.sql
+++ homework_6.sql
@@ -1,1 +1,12 @@
-USE vk;
+1. Пусть задан некоторый пользователь. Из всех пользователей соц. сети найдите человека, который больше всех общался с выбранным пользователем (написал ему сообщений).
+
+SELECT firstname, lastname from users WHERE id = (
+SELECT from_user_id 
+	FROM messages
+	WHERE to_user_id = 1
+	GROUP BY from_user_id
+	ORDER BY COUNT(from_user_id) DESC
+	LIMIT 1
+);
+
+
