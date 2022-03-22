ee510360e9e7acba3c0350c0bb74ac194d861899
Progul <desnenko@ya.ru>
2022-03-22 23:12:35 +0300

    added a second task

--- homework_6.sql
+++ homework_6.sql
@@ -1,7 +1,7 @@
 1. Пусть задан некоторый пользователь. Из всех пользователей соц. сети найдите человека, который больше всех общался с выбранным пользователем (написал ему сообщений).
 
 SELECT firstname, lastname from users WHERE id = (
-SELECT from_user_id 
+SELECT from_user_id
 	FROM messages
 	WHERE to_user_id = 1
 	GROUP BY from_user_id
@@ -9,4 +9,12 @@ SELECT from_user_id
 	LIMIT 1
 );
 
+2. Подсчитать общее количество лайков, которые получили пользователи младше 10 лет.
 
+SELECT COUNT(*) as 'likes count'
+FROM likes
+WHERE user_id IN (
+	SELECT user_id
+	FROM profiles
+	WHERE TIMESTAMPDIFF(YEAR, birthday, NOW()) < 10)
+;
