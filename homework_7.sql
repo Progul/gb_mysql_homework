5f88bb630d92e6d72d54e96bf5ddfc6dde96ae62
Progul <desnenko@ya.ru>
2022-03-26 10:12:33 +0300

    added 7 homework

--- homework_7.sql
+++ homework_7.sql
@@ -1,1 +1,17 @@
 --Тема “Сложные запросы”
+
+--1. Составьте список пользователей users,
+-- которые осуществили хотя бы один заказ orders в интернет магазине.
+
+SELECT u.name
+FROM users AS u
+INNER JOIN orders AS o ON (o.user_id = u.id)
+GROUP BY u.name
+HAVING COUNT(o.id) > 0
+--2. Выведите список товаров products и разделов catalogs,
+-- который соответствует товару.
+
+SELECT p.name, c.name
+FROM products AS p
+INNER JOIN catalogs AS c ON (p.catalog_id = c.id)
+GROUP BY p.id
